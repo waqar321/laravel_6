f83d2fd9a3db1ee3ae6dbaf8256dd869fc55a9c3
Waqar Mughal <waqarmughal707@gmail.com>
2020-01-02 17:18:32 +0500

    Update laravel_theory.php

--- laravel_theory.php
+++ laravel_theory.php
@@ -1282,4 +1282,52 @@ page not found:
  		    my dir: 			 C:\xampp\htdocs\laravel_6\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions\views
 
 //============================================================
+    PDF generate:
+                composer require barryvdh/laravel-dompdf
+            
+                add service:
+                                    Barryvdh\DomPDF\ServiceProvider::class,
+                add alias:
+                                    'PDF' => Barryvdh\DomPDF\Facade::class,
+    
+                1 - Route::get('generate-pdf','PDFController@generatePDF');
+                2-  PDFController
+                            <?php
+                                    namespace App\Http\Controllers;
+
+                                    use Illuminate\Http\Request;
+                                    use PDF;
+
+                                    class PDFController extends Controller
+                                    {
+                                        /**
+                                         * Display a listing of the resource.
+                                         *
+                                         * @return \Illuminate\Http\Response
+                                         */
+                                        public function generatePDF()
+                                        {
+                                            $data = ['title' => 'Welcome to ItSolutionStuff.com'];
+                                            $pdf = PDF::loadView('myPDF', $data);
+
+                                            return $pdf->download('itsolutionstuff.pdf');
+                                        }
+                                    }
+                                ?>
+                3 - BLADe
+                            <!DOCTYPE html>
+                            <html>
+                            <head>
+                                <title>Hi</title>
+                            </head>
+                            <body>
+                                <h1>Welcome to ItSolutionStuff.com - {{ $title }}</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </body>
+                            </html>
 
